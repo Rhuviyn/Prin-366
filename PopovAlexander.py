24840728141024de5b0ee8564f730167c6c7e1ed
Alexander Popov <pipo2017@mail.ru>
2026-03-16 12:20:45 +0300

    docs: add user schema description

--- PopovAlexander.py
+++ PopovAlexander.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from pydantic import BaseModel, Field
 
-
+# Base user schema for routers input
 class UserSchema(BaseModel):
     id: int
     nickname: str = Field(min_length=5, max_length=20)
