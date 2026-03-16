0387a947541ef192ddf8b3c6dd00824358edf1ed
Alexander Popov <pipo2017@mail.ru>
2026-03-16 12:35:32 +0300

    docs: add user registration schema description

--- PopovAlexander.py
+++ PopovAlexander.py
@@ -14,6 +14,6 @@ class UserLoginSchema(BaseModel):
     login: str = Field(min_length=5, max_length=20)
     password: str = Field(min_length=8, max_length=100)
 
-
+# Schema for user registration
 class UserRegisterSchema(UserLoginSchema):
     nickname: str = Field(min_length=5, max_length=20)
